8bbdc0beb6fa2dd70de5a93b9c2b4ce28654a47b
Fernando Ferreira <fernandoferreira@usp.br>
2024-09-27 17:43:12 -0300

    Adicionado integração com Bootstrap

--- scem/scem/settings.py
+++ scem/scem/settings.py
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
