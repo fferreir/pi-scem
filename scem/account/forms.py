56702ff78b8c3d1c30ef081811d85a517a6dbf34
Fernando Ferreira <fernandoferreira@usp.br>
2024-09-30 17:45:58 -0300

    Adicionadas páginas para alterar senha e recuperar senha

--- scem/account/forms.py
+++ scem/account/forms.py
@@ -1,6 +1,4 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
