155837d98cffc9519cbc2d4d91cee10c49658a06
Fernando Ferreira <fernandoferreira@usp.br>
2024-10-26 08:17:23 -0300

    Corrige o PATH da API

--- scem/scem/urls.py
+++ scem/scem/urls.py
@@ -26,7 +26,7 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
